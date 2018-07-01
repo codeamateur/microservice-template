c15f93fe550b5e882f56138466ac261493701606
zjx20 <network404@outlook.com>
2018-07-01 23:15:30 +0800

    commit

---
+++ template-account/src/main/java/com/zjx/dev/template/account/TemplateAccountApplication.java
@@ -0,0 +1,12 @@
+package com.zjx.dev.template.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateAccountApplication.class, args);
+	}
+}
